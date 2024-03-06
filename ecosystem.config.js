9facd565e59ec9c33e293f86e061ca9ae6ee7421
rcraftzy <roberto.tg99@gmail.com>
2024-03-06 20:50:35 +0000

    Change to update app

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,16 +2,12 @@ module.exports = {
   apps : [{
     name: 'api',
     script: 'index.js',
-    
     cwd: "/home/admin01/projects/api/src",
     autorestart: true,
     watch: false,
     max_memory_restart: '16',    
     env: {
-      NODE_ENV: 'development'
-    },
-    env: {
-      NODE_ENV: 'production'
+      NODE_ENV: 'production' // Definir correctamente el entorno de producción
     }
   }],
 
@@ -23,7 +19,7 @@ module.exports = {
       repo : 'git@github.com:rcraftzy/escp-api-rest',
       fetch: 'all',
       path : '/home/admin01/project',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'     
+      'post-deploy' : 'git fetch && git merge origin/main && npm install && pm2 reload ecosystem.config.js --env production'     
     }
   }
-};
+}
