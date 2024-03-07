7906e01564c52c2ab0b022650457e793b836d933
Teamcellmania <teamcellmania@Teamcellmanias-MacBook-Air.local>
2024-03-07 10:32:57 -0800

    script añadido para que se actualice el servidor

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 const express = require('express')
 const { print } = require("./example.js")
-// import path from 'path'
-// import { fileURLToPath } from 'url';
 
 const app = express();
-/* 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-*/
-// app.use(express.static(path.join(__dirname, '/dist')));
 
-app.get('/imprimir', (req, res) => print());
+app.get('/imprimir', (req, res) => {
+  const {ref} = req.body
+  if (ref === 'refs/heads/main') {
+    exec('git pull && npm install && pm2 restart api', (error) => {
+      if (error) return res.status(500).send("Error interno en la actualizacion")
+      res.status(200).send("Actualizado")
+    })
+  } else {
+    res.status(200).send("No se requiere actualizacion.")
+
+  }
+  return print()
+});
 app.get('/', (req, res) => {
   res.send("Hola mundo, actualizacion 4");
 });
