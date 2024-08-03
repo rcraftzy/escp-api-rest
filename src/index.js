a9bbae534df482224898c4fae2ac149c997719f2
Teamcellmania <teamcellmania@Teamcellmanias-MacBook-Air.local>
2024-08-03 15:35:42 -0500

    Update projecaat

--- src/index.js
+++ src/index.js
@@ -7,13 +7,8 @@ const app = express();
 app.get('/imprimir', (req, res) => {
   const {ref} = req.body
   if (ref === 'refs/heads/main') {
-    exec('cd /home/admin01/projects/current && pm2 deploy production', (error) => {
-      if (error) return res.status(500).send("Error interno en la actualizacion")
-      res.status(200).send("Actualizado")
-    })
   } else {
     res.status(200).send("No se requiere actualizacion.")
-
   }
   return print()
 });
